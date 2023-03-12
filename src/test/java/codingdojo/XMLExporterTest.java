f6ad8673d0e58225a1e9837332a2a0bdbb0680d7
Blazej Sewera <code@sewera.dev>
2023-03-13 00:01:56 +0100

    feat: inital setup

---
+++ src/test/java/codingdojo/XMLExporterTest.java
@@ -0,0 +1,15 @@
+package codingdojo;
+
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
+
+public class XMLExporterTest {
+
+    @Test
+    public void testSomething() {
+
+        // TODO: implement this test
+
+    }
+
+}
