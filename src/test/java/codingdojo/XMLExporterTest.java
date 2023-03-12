80ed08f5ed333113a2b7aac398a53d94fac04688
Blazej Sewera <code@sewera.dev>
2023-03-13 00:05:18 +0100

    test: refactor XMLExporterTest to be easier to read

--- src/test/java/codingdojo/XMLExporterTest.java
+++ src/test/java/codingdojo/XMLExporterTest.java
@@ -9,6 +9,7 @@ import static org.xmlunit.assertj3.XmlAssert.assertThat;
 
 
 public class XMLExporterTest {
+    public static final String TEST_XML_DIRECTORY = "src/test/resources/xml/";
 
     @Test
     public void testFullExport() {
@@ -19,12 +20,19 @@ public class XMLExporterTest {
         var order = new Order("1234", Util.fromIsoDate("2018-09-01T00:00Z"),
                 store, new Product[]{makeover});
 
+        var expectedXmlFile = "recentOrder.xml";
+
         // when
         var xml = XMLExporter.exportFull(List.of(order));
-        var expected = Input.fromFile("src/test/resources/xml/recentOrder.xml").build();
-        var actual = Input.fromString(xml).build();
 
         // then
+        assertXmlAgainstFile(xml, expectedXmlFile);
+    }
+
+    private static void assertXmlAgainstFile(String actualXml, String expectedXmlFile) {
+        var expected = Input.fromFile(TEST_XML_DIRECTORY + expectedXmlFile).build();
+        var actual = Input.fromString(actualXml).build();
+
         assertThat(actual).and(expected).ignoreWhitespace().areIdentical();
     }
 }
