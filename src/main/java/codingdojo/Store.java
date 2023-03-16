430d24cfe3e68a72a20ddc4915108327e89f8d47
Blazej Sewera <code@sewera.dev>
2023-03-16 23:42:49 +0100

    refactor: remove unnecessary methods from Store

--- src/main/java/codingdojo/Store.java
+++ src/main/java/codingdojo/Store.java
@@ -1,16 +1,12 @@
 package codingdojo;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Represents a physical Store where you can go and buy
  * products and attend events.
  */
 public class Store {
-
     private final Map<String, Product> itemsInStock = new HashMap<>();
     private final String name;
     private final String id;
@@ -22,21 +18,13 @@ public class Store {
     }
 
     public void addStockedItems(Product... items) {
-        for (Product item: items) {
+        for (Product item : items) {
             this.itemsInStock.put(item.getName(), item);
         }
     }
 
     public void addStoreEvent(StoreEvent storeEvent) {
         this.itemsInStock.put(storeEvent.getName(), storeEvent);
-    }
-
-    public boolean hasItem(Product item) {
-        return itemsInStock.containsKey(item.getName());
-    }
-
-    public Product getItem(String name) {
-        return itemsInStock.get(name);
     }
 
     public String getId() {
@@ -48,15 +36,16 @@ public class Store {
         return "Store{" + name + '}';
     }
 
-    public void saveToDatabase() {
-        throw new UnsupportedOperationException("missing from this exercise - shouldn't be called from a unit test");
-    }
-
     public String getName() {
         return name;
     }
 
     public Collection<Product> getStock() {
         return Collections.unmodifiableCollection(this.itemsInStock.values());
+    }
+
+    @SuppressWarnings("unused")
+    public void saveToDatabase() {
+        throw new UnsupportedOperationException("missing from this exercise - shouldn't be called from a unit test");
     }
 }
