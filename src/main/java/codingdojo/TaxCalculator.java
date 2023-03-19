f5957fb8503c28b930a7381f88e8be431f5be9ae
Blazej Sewera <code@sewera.dev>
2023-03-19 17:01:19 +0100

    refactor: extract tax for a single order calculation

--- src/main/java/codingdojo/TaxCalculator.java
+++ src/main/java/codingdojo/TaxCalculator.java
@@ -6,18 +6,23 @@ public class TaxCalculator {
     private static final Date TAX_CHANGE = DateUtil.fromIsoDate("2018-01-01T00:00Z");
 
     public static double calculateAddedTax(Collection<Order> orders) {
-        double tax = 0.0;
-        for (Order order : orders) {
-            tax += orderTaxFor(order);
-
-            for (Product product : order.products())
-                tax += product.getTax();
+        var tax = 0.0;
+        for (var order : orders) {
+            tax += taxForOrder(order);
         }
 
         return tax;
     }
 
-    private static double orderTaxFor(Order order) {
+    private static double taxForOrder(Order order) {
+        var tax = initialTaxForOrder(order);
+
+        for (var product : order.products())
+            tax += product.getTax();
+        return tax;
+    }
+
+    private static double initialTaxForOrder(Order order) {
         return order.date().before(TAX_CHANGE) ? 10.0 : 20.0;
     }
 }
