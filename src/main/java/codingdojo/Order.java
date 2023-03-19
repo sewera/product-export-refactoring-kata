76815f284c0ac4598ffbf23095434de97da48482
Blazej Sewera <code@sewera.dev>
2023-03-19 17:12:01 +0100

    refactor: add Order.getTax

--- src/main/java/codingdojo/Order.java
+++ src/main/java/codingdojo/Order.java
@@ -5,10 +5,22 @@ import java.util.*;
 import static codingdojo.Price.Currency.USD;
 
 public record Order(String id, Date date, Store store, Product[] products) {
+    private static final Date TAX_CHANGE = DateUtil.fromIsoDate("2018-01-01T00:00Z");
+
     public double totalDollars() {
         return Arrays.stream(products)
                 .mapToDouble(product -> product.getPriceInCurrency(USD))
                 .sum();
+    }
+
+    public double getTax() {
+        return initialTax() + Arrays.stream(products)
+                .mapToDouble(Product::getTax)
+                .sum();
+    }
+
+    public double initialTax() {
+        return date.before(TAX_CHANGE) ? 10.0 : 20.0;
     }
 
     @SuppressWarnings("unused")
