1eb0c739b794e11891180068a7361401e446e819
Blazej Sewera <code@sewera.dev>
2023-03-19 16:27:40 +0100

    refactor: extract TaxCalculator.TAX_CHANGE

--- src/main/java/codingdojo/TaxCalculator.java
+++ src/main/java/codingdojo/TaxCalculator.java
@@ -3,10 +3,12 @@ package codingdojo;
 import java.util.*;
 
 public class TaxCalculator {
+    private static final Date TAX_CHANGE = DateUtil.fromIsoDate("2018-01-01T00:00Z");
+
     public static double calculateAddedTax(Collection<Order> orders) {
         double tax = 0.0;
         for (Order order : orders) {
-            if (order.date().before(DateUtil.fromIsoDate("2018-01-01T00:00Z")))
+            if (order.date().before(TAX_CHANGE))
                 tax += 10;
             else
                 tax += 20;
